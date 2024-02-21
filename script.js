233692ade70f1f55156e30b23c6b0f6cdae177f6
JessicaDabennini <147037874+JessicaDabennini@users.noreply.github.com>
2024-02-21 18:02:51 +0100

    end

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let counterResult = document.querySelector('[data-counter]');
 let minusBtn = document.querySelector('#minus');
 let plusBtn = document.querySelector('#plus');
@@ -22,4 +23,30 @@ function sub(){
 function res(){
     value = 0;
     counterResult.innerHTML =  value;
+=======
+let counterResult = document.querySelector('[data-counter]');
+let minusBtn = document.querySelector('#minus');
+let plusBtn = document.querySelector('#plus');
+let resetBtn = document.querySelector('#resetBtn');
+
+plusBtn.addEventListener ('click', add);
+minusBtn.addEventListener ('click', sub);
+resetBtn.addEventListener ('click', res);
+
+let value = 0;
+
+function add(){
+    value++;
+    counterResult.innerHTML = value;
+}
+
+function sub(){
+    value--;
+    counterResult.innerHTML = value;
+} 
+
+function res(){
+    value = 0;
+    counterResult.innerHTML =  value;
+>>>>>>> ab4d27a (end)
 }
