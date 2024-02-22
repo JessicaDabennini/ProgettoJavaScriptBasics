4cc024232e4da71cfc5aa3ecac89417ff3fd6e45
Jessica Dabennini <147037874+JessicaDabennini@users.noreply.github.com>
2024-02-23 00:03:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let counterResult = document.querySelector('[data-counter]');
 let minusBtn = document.querySelector('#minus');
 let plusBtn = document.querySelector('#plus');
+let resetBtn = document.querySelector('#resetBtn');
 
 plusBtn.addEventListener ('click', add);
 minusBtn.addEventListener ('click', sub);
+resetBtn.addEventListener ('click', res);
 
 let value = 0;
 
@@ -15,4 +17,9 @@ function add(){
 function sub(){
     value--;
     counterResult.innerHTML = value;
+} 
+
+function res(){
+    value = 0;
+    counterResult.innerHTML =  value;
 }
