5454a46e2fdda64975a2245f8b657bd10b32d0b1
JessicaDabennini <jessica.dabennini@gmail.com>
2024-02-27 10:45:06 +0100

    commit changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let counterResult = document.querySelector('[data-counter]');
 let minusBtn = document.querySelector('#minus');
 let plusBtn = document.querySelector('#plus');
@@ -23,31 +22,4 @@ function sub(){
 function res(){
     value = 0;
     counterResult.innerHTML =  value;
-}
-=======
-let counterResult = document.querySelector('[data-counter]');
-let minusBtn = document.querySelector('#minus');
-let plusBtn = document.querySelector('#plus');
-let resetBtn = document.querySelector('#resetBtn');
-
-plusBtn.addEventListener ('click', add);
-minusBtn.addEventListener ('click', sub);
-resetBtn.addEventListener ('click', res);
-
-let value = 0;
-
-function add(){
-    value++;
-    counterResult.innerHTML = value;
-}
-
-function sub(){
-    value--;
-    counterResult.innerHTML = value;
-} 
-
-function res(){
-    value = 0;
-    counterResult.innerHTML =  value;
->>>>>>> ab4d27a (end)
 }
